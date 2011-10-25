93d83658b1fac50bf675b399272964b85b372d30
huangtao <huangtao@jinke-pc.(none)>
2011-10-25 13:42:00 +0800

    return values changed to Boolean

--- linklist_base.c
+++ linklist_base.c
@@ -21,11 +21,11 @@ void linklist_destory(struct LinkList **L) {
 
 Int32 linklist_clear(struct LinkList **L) {
 	if (!*L)
-		return ERROR;
+		return false;
 	struct LinkList *p = (*L)->next;
 	(*L)->next = NULL;
 	linklist_destory(&p);
-	return SUCCEED;
+	return true;
 }
 
 Int32 linklist_length(struct LinkList *L) {
@@ -60,9 +60,9 @@ Int32 linklist_show_node(struct LinkList *L) {
 Int32 linklist_get_pos_elem(struct LinkList *L, int pos, LinkElement *e) {
 	struct LinkList *node = linklist_get_pos_node(L,pos);
 	if (!node)
-		return ERROR;
+		return false;
 	*e = node->data;
-	return SUCCEED;
+	return true;
 }
 
 struct LinkList* linklist_get_pos_node(struct LinkList *L, Int32 pos) {
@@ -92,27 +92,27 @@ struct LinkList* linklist_get_last_node(struct LinkList *L) {
 	return last;
 }
 
-Int32 linklist_insert_elem(struct LinkList **L, Int32 pos, LinkElement e) {
+Boolean linklist_insert_elem(struct LinkList **L, Int32 pos, LinkElement e) {
 	if (pos < 0)
-		return ERROR;
+		return false;
 	struct LinkList *p;
 	p = pos > 0? linklist_get_pos_node(*L,pos - 1 < 0? 0:pos - 1) : *L;
 	if (!p)
-		return ERROR;
+		return false;
 
 	struct LinkList *new = (struct LinkList*)malloc(sizeof(struct LinkList));
 	if (!new)
-		return ERROR;
+		return false;
 	new->data = e;
 	new->next = p->next;
 	p->next = new;
 
-	return SUCCEED;
+	return true;
 }
 
-Int32 linklist_remove(struct LinkList **L, Int32 pos) {
+Boolean linklist_remove(struct LinkList **L, Int32 pos) {
 	if (pos < 0)
-		return ERROR;
+		return false;
 	struct LinkList *p, *q;
 
 	// get prev node
@@ -121,25 +121,25 @@ Int32 linklist_remove(struct LinkList **L, Int32 pos) {
 	else
 		p = linklist_get_pos_node(*L,pos - 1);
 	if (!p)
-		return ERROR;
+		return false;
 	q = p->next;
 	if (!q)
-		return ERROR;
+		return false;
 	p->next = q->next;
 	free(q);
 
-	return SUCCEED;
+	return true;
 }
 
-Int32 linklist_append_elem(struct LinkList **L, LinkElement e) {
+Boolean linklist_append_elem(struct LinkList **L, LinkElement e) {
 	if (!*L)
-		return ERROR;
+		return false;
 	return linklist_insert_elem(L, linklist_length(*L), e);
 }
 
-Int32 linklist_join(struct LinkList **dest, struct LinkList **src, Int32 pos) {
+Boolean linklist_join(struct LinkList **dest, struct LinkList **src, Int32 pos) {
 	if (!(*dest) || !(*src) || !(*src)->next || pos < 0)
-		return ERROR;
+		return false;
 	struct LinkList *prev;
 	struct LinkList *src_peeled = (*src)->next, *src_last;
 	if (pos == 0)
@@ -147,19 +147,19 @@ Int32 linklist_join(struct LinkList **dest, struct LinkList **src, Int32 pos) {
 	else
 		prev = linklist_get_pos_node(*dest, pos - 1);
 	if (!prev)
-		return ERROR;
+		return false;
 	Int32 src_length = linklist_length(*src);
 	src_last = linklist_get_pos_node(*src, src_length - 1);
 	if (!src_last)
-		return ERROR;
+		return false;
 	src_last->next = prev->next;
 	prev->next = src_peeled;
-	return SUCCEED;
+	return true;
 }
 
-Int32 linklist_insert_node(struct LinkList **L, struct LinkList *node, Int32 pos) {
+Boolean linklist_insert_node(struct LinkList **L, struct LinkList *node, Int32 pos) {
 	if (!(*L) || !node)
-		return ERROR;
+		return false;
 
 	struct LinkList *last = linklist_get_last_node(node);
 	struct LinkList *prev;
@@ -168,8 +168,8 @@ Int32 linklist_insert_node(struct LinkList **L, struct LinkList *node, Int32 pos
 	else
 		prev = linklist_get_pos_node(*L,pos - 1);
 	if (!prev)
-		return ERROR;
+		return false;
 	last->next = prev->next;
 	prev->next = node;
-	return SUCCEED;
+	return true;
 }
--- linklist_base.h
+++ linklist_base.h
@@ -9,27 +9,27 @@
 void linklist_create(struct LinkList **L);
 /// destory all nodes include head node
 void linklist_destory(struct LinkList **L);
-/// destory all nodes but head node, ERROR returns if NULL list
+/// destory all nodes but head node, false returns if NULL list
 Int32 linklist_clear(struct LinkList **L);
 /// get link list length without head node
 Int32 linklist_length(struct LinkList *L);
 /// append element to tail of link list
-Int32 linklist_append_elem(struct LinkList **L, LinkElement e);
-/// get value of link list on pos, ERROR returns if failed
+Boolean linklist_append_elem(struct LinkList **L, LinkElement e);
+/// get value of link list on pos, false returns if failed
 Int32 linklist_get_pos_elem(struct LinkList *L, int pos, LinkElement *e);
 /// get link list node on pos, NULL if failed
 struct LinkList* linklist_get_pos_node(struct LinkList *L, Int32 pos);
 /// get the last node in list
 struct LinkList* linklist_get_last_node(struct LinkList *L);
-/// insert elem into link list on pos, ERROR returns if failed
-Int32 linklist_insert_elem(struct LinkList **L, Int32 pos, LinkElement e);
-/// remove node on pos, ERROR returns if failed
-Int32 linklist_remove(struct LinkList **L, Int32 pos);
+/// insert elem into link list on pos, false returns if failed
+Boolean linklist_insert_elem(struct LinkList **L, Int32 pos, LinkElement e);
+/// remove node on pos, false returns if failed
+Boolean linklist_remove(struct LinkList **L, Int32 pos);
 /// join another link list with head node into list dest
 //  peel the head node of src, then join
-Int32 linklist_join(struct LinkList **dest, struct LinkList **src, Int32 pos);
+Boolean linklist_join(struct LinkList **dest, struct LinkList **src, Int32 pos);
 /// insert node or linked nodes to link list
-Int32 linklist_insert_node(struct LinkList **L, struct LinkList *node, Int32 pos);
+Boolean linklist_insert_node(struct LinkList **L, struct LinkList *node, Int32 pos);
 
 /// traverse in link list in sequence, NOT includes head node!
 // traverse stops immediately if f returns less than 0
